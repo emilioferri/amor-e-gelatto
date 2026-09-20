4a6beec76ce45630a94fb7a137e64d73cb8d2364
emilioferri <emilioferri@gmail.com>
2026-09-20 15:23:59 -0300

    Sync: config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+﻿/**
+ * AMOR E GELATTO - CONFIGURACAO DO SUPABASE (SINCRONIZADA)
+ */
+window.AMOR_E_GELATTO_CONFIG = {
+  SUPABASE_URL: 'https://azymydyzdbbeiqitzkyl.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_85Yt8_gtrSIYI5PFc4YONQ_cgEoGFUV',
+  isSupabaseConfigured() {
+    return Boolean(this.SUPABASE_URL && this.SUPABASE_ANON_KEY && this.SUPABASE_URL.startsWith('https://'));
+  }
+};
